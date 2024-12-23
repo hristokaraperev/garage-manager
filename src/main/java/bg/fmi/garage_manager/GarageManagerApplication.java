764cd8cc6ed27ffcaa8da03bed57aa69c6c86cdc
Hristo Karaperev <hristokaraperev@gmail.com>
2024-12-23 15:16:11 +0200

    project initialization

---
+++ src/main/java/bg/fmi/garage_manager/GarageManagerApplication.java
@@ -0,0 +1,13 @@
+package bg.fmi.garage_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/fmi/garage_manager/GarageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.fmi.garage_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
